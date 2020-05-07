4cbaf9c51bd5a7ebbf5680a18ed68d9c45286838
Javad Meymari <j.meymari@TNC.COM>
2020-05-07 17:37:18 +0430

    By Meymari

---
+++ Commit.bat
@@ -0,0 +1,12 @@
+echo "# Boors" >> README.md
+set gitdir=C:\Users\j.meymari\Downloads\PortableGit
+
+C:\Users\j.meymari\Downloads\PortableGit\bin\git status
+pause
+C:\Users\j.meymari\Downloads\PortableGit\bin\git add -A
+pause
+C:\Users\j.meymari\Downloads\PortableGit\bin\git commit -m "By Meymari"
+pause
+C:\Users\j.meymari\Downloads\PortableGit\bin\git remote add origin https://github.com/meymari/Sandogh.git
+C:\Users\j.meymari\Downloads\PortableGit\bin\git push -u origin master
+pause
---
+++ README.md
@@ -0,0 +1,2 @@
+"# Boors" 
+"# Boors" 
