51c02c4a7fdc426f6cc1285358064d2e00e32a24
جواد کلاته میمری <meymari@behsazan.local>
2020-05-08 14:55:52 +0430

    By Meymari

--- Commit-behsazan.bat
+++ Commit-behsazan.bat
@@ -8,6 +8,5 @@ pause
 pause
 \\storage.behsazan.local\meymari\PortableGit\bin\git -C \\storage.behsazan.local\meymari\Sandogh commit -m "By Meymari"
 pause
-\\storage.behsazan.local\meymari\PortableGit\bin\git -C \\storage.behsazan.local\meymari\Sandogh remote add origin https://github.com/meymari/Sandogh.git
-C:\Users\j.meymari\Downloads\PortableGit\bin\git push -u origin master
+\\storage.behsazan.local\meymari\PortableGit\bin\git push -u origin master
 pause
