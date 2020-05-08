f3c94d1db30ac51550688c2bd60fc83c8ff54373
جواد کلاته میمری <meymari@behsazan.local>
2020-05-08 15:03:08 +0430

    By Meymari

--- Commit-behsazan.bat
+++ Commit-behsazan.bat
@@ -5,3 +5,4 @@ REM pause
 \\storage.behsazan.local\meymari\PortableGit\bin\git -C \\storage.behsazan.local\meymari\Sandogh add -A
 \\storage.behsazan.local\meymari\PortableGit\bin\git -C \\storage.behsazan.local\meymari\Sandogh commit -m "By Meymari"
 \\storage.behsazan.local\meymari\PortableGit\bin\git -C \\storage.behsazan.local\meymari\Sandogh push -u origin master
+pause
