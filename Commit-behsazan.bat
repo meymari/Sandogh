07c4abe74bdead4a853e68abe0213d82c442e84c
جواد کلاته میمری <meymari@behsazan.local>
2020-05-09 15:00:43 +0430

    By Meymari

--- Commit-behsazan.bat
+++ Commit-behsazan.bat
@@ -2,6 +2,8 @@ REM \\storage.behsazan.local\meymari\PortableGit\bin\git config --system http.ss
 REM \\storage.behsazan.local\meymari\PortableGit\bin\git -C \\storage.behsazan.local\meymari\Sandogh clone https://github.com/meymari/Sandogh.git
 REM pause
 \\storage.behsazan.local\meymari\PortableGit\bin\git -C \\storage.behsazan.local\meymari\Sandogh status
+\\storage.behsazan.local\meymari\PortableGit\bin\git -C \\storage.behsazan.local\meymari\Sandogh add "Sandogh.exe"
+\\storage.behsazan.local\meymari\PortableGit\bin\git -C \\storage.behsazan.local\meymari\Sandogh add "DB.mdb"
 \\storage.behsazan.local\meymari\PortableGit\bin\git -C \\storage.behsazan.local\meymari\Sandogh add -A
 \\storage.behsazan.local\meymari\PortableGit\bin\git -C \\storage.behsazan.local\meymari\Sandogh commit -m "By Meymari"
 \\storage.behsazan.local\meymari\PortableGit\bin\git -C \\storage.behsazan.local\meymari\Sandogh push -u origin master
