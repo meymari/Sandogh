97728732c2392f19e1cf264aafcdbea26fce6530
جواد کلاته میمری <meymari@behsazan.local>
2020-05-27 18:38:51 +0430

    At Behsazan

--- Pull-behsazan.bat
+++ Pull-behsazan.bat
@@ -1,4 +1,2 @@
-\\storage.behsazan.local\meymari\PortableGit\bin\git --help
-pause
 \\storage.behsazan.local\meymari\PortableGit\bin\git -C \\storage.behsazan.local\meymari\Sandogh pull
 pause
---
+++ Reset-behsazan.bat
@@ -0,0 +1,2 @@
+\\storage.behsazan.local\meymari\PortableGit\bin\git -C \\storage.behsazan.local\meymari\Sandogh checkout .
+pause
