7c1309d0331150425b71a9b5200e2109344f184b
جواد کلاته میمری <meymari@behsazan.local>
2020-06-24 18:02:38 +0430

    At Behsazan

--- sp_fund_volume_chart.sql
+++ sp_fund_volume_chart.sql
@@ -7,7 +7,7 @@ BEGIN
     DELETE FROM fund_share    WHERE deleted = 1;
     DELETE FROM fund_asset    WHERE deleted = 1;
     DELETE FROM symbol_value  WHERE deleted = 1;
-    
+	#------------------------------------------------------------------------      
 	SET @l_min_date_industry = (SELECT COALESCE(MIN(date),(SELECT MAX(date) from fund_industry)) from fund_industry where chart is null or chart = false);
 	SET @l_min_date_share    = (SELECT COALESCE(MIN(date),(SELECT MAX(date) from fund_share   )) from fund_share    where chart is null or chart = false);
 	SET @l_min_date_asset    = (SELECT COALESCE(MIN(date),(SELECT MAX(date) from fund_asset   )) from fund_asset    where chart is null or chart = false);
@@ -30,7 +30,6 @@ BEGIN
 	IF (@l_min_date < '1399/01/01') THEN
       SET @l_min_date = '1399/01/01';
     END IF;
-	
 	SELECT 
 		@l_min_date as l_min_date, 
 		@l_min_date_industry AS l_min_date_industry, 
@@ -49,9 +48,9 @@ BEGIN
 		@l_max_date_share AS l_max_date_share, 
 		@l_max_date_asset AS l_max_date_asset, 
 		@l_max_date_symbol AS l_max_date_symbol;
-
+	#------------------------------------------------------------------------  	
     DELETE FROM fund_volume_chart WHERE `date` >= @l_min_date;
-	     
+	#------------------------------------------------------------------------  	     
 	UPDATE fund_share AS fs 
     SET nav_stock = (
 		SELECT fs.NAV*fund_asset.Stock/100 FROM fund_asset
@@ -62,25 +61,44 @@ BEGIN
     UPDATE fund_share AS fs 
 	SET ratio = fs.nav_stock /(SELECT sum(nav_stock) FROM fund_share WHERE fund_share.DATE = fs.DATE and fund_share.deleted = 0)* 100
     WHERE fs.DATE >= @l_min_date;
-	
+	#------------------------------------------------------------------------  
 	CALL sp_set_industry_code();
-	
-    UPDATE fund_industry AS fi
-	SET ratio = (
-		SELECT float_ratio FROM vw_power_industry 
-		WHERE 
-			vw_power_industry.date = fi.date and 
-			vw_power_industry.fund_code = fi.fund_code and
-			vw_power_industry.industry_code = fi.industry_code		
+	#------------------------------------------------------------------------  
+	DELETE FROM industry_date WHERE symbol_value.date >= '1399/01/01';
+	INSERT INTO industry_date(industry_code, date, value) 
+	SELECT industry.code, symbol_value.date, SUM(symbol_value.float_stock * symbol_value.price_complete)
+	FROM
+	  symbol_value join 
+	  symbol on(symbol_value.symbol_code = symbol.code) join 
+	  industry on(symbol.industry_code = industry.code) 
+	WHERE 
+	  symbol_value.date >= '1399/01/01'
+	GROUP BY industry.code, symbol_value.date  
+	#------------------------------------------------------------------------  	
+	UPDATE fund_industry AS fi
+	SET 
+	power_ratio = (
+		(
+			SELECT fi.industry_percent * nav_stock / 100 FROM fund_share
+			WHERE fund_code = fi.fund_code AND date = fi.date and deleted = 0
+		)/
+		(
+			SELECT value FROM industry_date
+			WHERE industry_code = fi.industry_code AND date = fi.date
+		)*100
 	)
-    WHERE fs.DATE >= @l_min_date;	
-   
-	INSERT INTO fund_volume_chart(`date`,industry_code,ratio)
-	SELECT fi.`date`, industry_code, ROUND(SUM(industry_ratio),1) AS ratio
-	FROM vw_fund_industry fi
+	,volume_ratio = (
+		SELECT fi.industry_percent * fund_share.ratio FROM fund_share
+		WHERE fund_code = fi.fund_code AND date = fi.date and deleted = 0
+	)
+	WHERE fi.date > @l_min_date and fi.deleted = 0
+	#------------------------------------------------------------------------  
+	INSERT INTO fund_volume_chart(`date`,industry_code,volume,power)
+	SELECT fi.`date`, industry_code, ROUND(SUM(volume_ratio),1) AS volume,ROUND(SUM(volume_ratio),1) AS power
+	FROM fund_industry fi
     WHERE fi.Date >= @l_min_date AND fi.type Like '%A%'
 	GROUP BY fi.`date`, industry_code; 
-    
+	#------------------------------------------------------------------------  
 	UPDATE `fund_industry` SET `chart`=true
 	where (`date` >= @l_min_date) AND (created_at <= @l_max_date_industry);
     
@@ -109,62 +127,3 @@ BEGIN
 	SELECT "End sp_fund_volume_chart";	
 END$$
 DELIMITER ;
-
-
--------------------------------
-
-	
-
-
-
-
-UPDATE fund_industry AS fi
-SET ratio = (
-	(
-		SELECT fi.industry_percent * fund_share.nav_stock / 100	FROM fund_share
-		WHERE 
-			fund_share.fund_code = fi.fund_code AND 
-			fund_share.date = fi.date
-	)/
-	(
-		SELECT SUM(symbol_value.float_stock * symbol_value.price_complete)
-		FROM
-			symbol_value join 
-			symbol on(symbol_value.symbol_code = symbol.code) join 
-			industry on(symbol.industry_code = industry.code) 
-		WHERE 
-			industry.code = fi.industry_code AND symbol_value.date = fi.date
-	)*100
-)
-WHERE fi.date >= '1399/03/20';
-
---------------------------------
-
-
-UPDATE fund_industry AS fi
-SET ratio = (
-	(
-		SELECT fi.industry_percent * fund_share.nav_stock / 100	FROM fund_share
-		WHERE 
-			fund_share.fund_code = fi.fund_code AND 
-			fund_share.date = fi.date and fund_share.deleted = 0
-	)/
-	(
-		SELECT value FROM industry_date
-		WHERE industry_date.industry_code = fi.industry_code AND date = fi.date
-	)*100
-)
-WHERE fi.date > '1399/03/20' and fi.deleted = 0;
-
--------------------------------
-
-INSERT INTO `industry_date`(`industry_code`, `date`, `value`) 
-SELECT industry.code, symbol_value.date, SUM(symbol_value.float_stock * symbol_value.price_complete)
-FROM
-  symbol_value join 
-  symbol on(symbol_value.symbol_code = symbol.code) join 
-  industry on(symbol.industry_code = industry.code) 
-WHERE 
-  symbol_value.date >= '1399/01/01'
-GROUP BY industry.code, symbol_value.date  
------------------------
